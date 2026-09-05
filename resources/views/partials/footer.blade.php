934427353976b4d853583effae13533fef9524ed
VighneshN <vighneshnikhar16@gmail.com>
2026-09-05 19:18:43 +0530

    footer log issue

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,7 +12,6 @@
             <a href="{{ route('home') }}" class="inline-flex items-center mb-3" aria-label="{{ $__settings->site_name ?? config('app.name') }}">
                 @if ($__hasLogo)
                     <img src="{{ $__logoUrl }}" alt="{{ $__settings->site_name ?? config('app.name') }}" class="h-20 w-auto" style="filter: brightness(0) invert(1);">
-                    <span class="sr-only">{{ $__settings->site_name ?? config('app.name') }}</span>
                 @else
                     <div class="text-lg font-bold text-white">{{ $__settings->site_name ?? config('app.name') }}</div>
                 @endif
